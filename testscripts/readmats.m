842ccf44b354510330aa95b7c8ca9e55a39c3a0e
ndoyle815 <u2260253@live.warwick.ac.uk>
2023-09-04 13:14:07 +0100

    removed early work from repo

--- testscripts/readmats.m
+++
@@ -1,7 +0,0 @@
-clear all
-
-load('../mats/metrics.mat')
-
-format long g
-Ss = ['S1'; 'S2'; 'S3'; 'S4'; 'S5'; 'S6'];
-mymetrics = [Peak_incidence', Peak_hospital', FinalSize', FinalHospital', Last_lockdown', Days_lockdown', NPhases']
